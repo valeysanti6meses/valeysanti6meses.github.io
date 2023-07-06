c9ab362835942df42227c6d6996549abfd240944
valeysanti6meses <138822028+valeysanti6meses@users.noreply.github.com>
2023-07-06 17:20:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function uno (){ window.location.href = "unmes.html"; }
+function dos (){ window.location.href = "dosmeses.html"; }
+function tres (){ window.location.href = "tresmeses.html";}
+function cinco (){ window.location.href = "cincomeses.html"; }
+function seis (){ window.location.href = "seismeses.html";}
+function cuatro (){ window.location.href = "cuatromeses.html"; }
+function volver (){ window.location.href = "menu.html"; }
